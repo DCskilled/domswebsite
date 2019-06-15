de2c5248c63b5cd5a474bf63a66bbc807d889d90
DCskilled <50896587+DCskilled@users.noreply.github.com>
2019-06-14 20:25:28 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+function changeImage() {
+document.getElementById("lightbulb").src="https://i.dailymail.co.uk/i/pix/2014/01/26/article-2546363-1AFB247500000578-934_306x423.jpg";
+}
+
+function resetImg() {
+document.getElementById("lightbulb").src="http://images.clipartpanda.com/light-bulb-png-light_bulb.png";
+
+}
